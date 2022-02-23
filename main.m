21fda841c7a4739864326bc93013cddd1834a4ed
Shivam Chaudhary <shivamc021999@gmail.com>
2022-02-23 13:48:11 +0530

    initial commit

---
+++ main.m
@@ -0,0 +1,18 @@
+%%
+% Topic: Representing a Limb's Resistance to Sudden Movement
+% Author: Shivam Chaudhary
+%         Indian Institute of Technology Gandhinagar
+%         shivamchaudhary@iitgn.ac.in
+% Date: 23 Feb 2022
+
+%% Activities
+% Consider a two-link planar representation of the limb with one actuation 
+% redundancy, i.e., n = 2 and m = 3, to work out the following points.
+%   1. Consider a mechanical extension spring model of the muscle and 
+%      develop expressions for the muscle space stiffness matrix. Develop 
+%      the joint-space stiffness matrix and task-space stiffness matrix.
+%   2. Using some numerical values for stiffness offered by each mechanical 
+%      spring, compute the task space and joint space stiffness matrices 
+%      when no external force is applied at the limb. I will compute task 
+%      and joint space ellipses when the arm is performing a reaching task
+%      and create an animation out of the movement.
