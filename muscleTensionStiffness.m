1b9edd378709af770ac2256e6c59eb426f008a97
Shivam Chaudhary <shivamc021999@gmail.com>
2022-02-27 21:37:56 +0530

    Kq computation done

--- muscleTensionStiffness.m
+++ muscleTensionStiffness.m
@@ -1,9 +1,8 @@
-function [Kd_sym] = muscleTensionStiffness(A_sym)
+function [Kd_sym] = muscleTensionStiffness(A_sym, Tension)
 %MUSCLETENSIONSTIFFNESS This function takes in symbolic structure matrix
 %and gives muscle tension stiffness equation.
 
-syms Q1 Q2 T1 T2 T3;
-T = [T1; T2; T3];
-Kd_sym = [diff(A_sym, Q1) * T diff(A_sym, Q2) * T];
+syms Q1 Q2;
+Kd_sym = [diff(A_sym, Q1) * Tension diff(A_sym, Q2) * Tension];
 end
 
